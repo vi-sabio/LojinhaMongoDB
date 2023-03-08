351b9840413cc6402b0e4a2d9956f699946126d0
Vinicius Sabio <vinicios_sabio@hotmail.com>
2023-03-08 20:06:26 -0300

    classe Product na pasta Collections feita

---
+++ LojinhaServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+using LojinhaServer.Models;
+using MongoDB.Driver;
+
+namespace LojinhaServer.Extensions;
+
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+        public static void ConfigureMongoDBSettings (this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<MongoDBSettings>(
+                config.GetSection("MongoDBSettings")
+            );
+
+            services.AddSingleton<IMongoDatabase>(options => {
+                var settings = config.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+                var client = new MongoClient(settings.ConnectionString);
+                return client.GetDatabase(settings.DatabaseName);
+
+            });
+        }
+        
+    }
