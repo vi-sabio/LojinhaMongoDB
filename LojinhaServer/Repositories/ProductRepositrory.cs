2de71d7d2c2d7af191d594516c3b2d440b290348
Vinicius Sabio <vinicios_sabio@hotmail.com>
2023-03-09 20:27:44 -0300

    fazer atividade 16

---
+++ LojinhaServer/Repositories/ProductRepositrory.cs
@@ -0,0 +1,29 @@
+using LojinhaServer.Collections;
+using MongoDB.Driver;
+
+namespace LojinhaServer.Repositories;
+
+    public class ProductRepositrory : IProductRepository
+    {
+        private readonly IMongoCollection<Product> _collection;
+
+        public ProductRepositrory(IMongoDatabase MongoDatabase)
+        {
+            _collection = MongoDatabase.GetCollection<Product>("products");
+        }
+
+        public async Task<List<Product>> GetAllAsync() =>
+            await _collection.Find(_ => true).ToListAsync();
+
+        public async Task<Product> GetByIdAsync(string id) =>
+            await _collection.Find(_ => _.Id == id) .FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Product product) =>
+            await _collection.InsertOneAsync(product);
+
+        public async Task UpdateAsync(Product product) =>
+            await _collection.ReplaceOneAsync(x => x.Id == product.Id, product);
+
+        public async Task DeleteAsync(string id) =>
+            await _collection.DeleteOneAsync(x => x.Id == id);
+    }
